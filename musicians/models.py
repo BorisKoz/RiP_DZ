7aca8d0054715855e3a17b0e87f67b2990ca1940
BorisKoz <55689768+BorisKoz@users.noreply.github.com>
2021-01-12 13:01:29 +0300

    admin translation

--- musicians/models.py
+++ musicians/models.py
@@ -6,18 +6,24 @@ from django.db import models
 
 class Musician(models.Model):
     Musician_id = models.AutoField(primary_key=True, verbose_name="ID музыканта")
-    Musician_name = models.CharField(max_length=100)
-    Site = models.CharField(max_length=200)
+    Musician_name = models.CharField(max_length=100, verbose_name="Имя музыканта/название группы")
+    Site = models.CharField(max_length=200, verbose_name="Сайт музыканта")
+
+    class Meta:
+        verbose_name = "Исполнитель"
+        verbose_name_plural = "Исполнители"
+
 
 class Song(models.Model):
-    Song_id = models.AutoField(primary_key=True)
-    Musician_key = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    slug = models.SlugField(default=1)
-    Song_name = models.CharField(max_length=100)
-    duration = models.CharField(max_length=50)
-    img = models.CharField(max_length=100)
-    lyrics = models.TextField()
-    song_link = models.CharField(max_length=200, default='ссылка на mp3')
+    Song_id = models.AutoField(primary_key=True, verbose_name="ID песни")
+    Musician_key = models.ForeignKey(Musician, on_delete=models.CASCADE, verbose_name="Исполнитель песни")
+    slug = models.SlugField(default=1, verbose_name="ID исполнителя")
+    Song_name = models.CharField(max_length=100, verbose_name="Название песни")
+    duration = models.CharField(max_length=50, verbose_name="Длительность песни")
+    img = models.CharField(max_length=100, verbose_name="Файл обложки")
+    lyrics = models.TextField(verbose_name="Текст песни")
+    song_link = models.CharField(max_length=200, default='ссылка на mp3', verbose_name="Файл песни")
 
-
-
+    class Meta:
+        verbose_name = "Песня"
+        verbose_name_plural = "Песни"
